fa945767912b0bb1b6bb6897311a45fad35da660
mangeshkanawade <kanawade84@gmail.com>
2025-09-23 18:53:17 +0530

    Bug Fixes.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  // apiUrl: 'https://api-payinvoflow.vercel.app/api/',
-  apiUrl: 'http://localhost:3000/api/',
+  apiUrl: 'https://api-payinvoflow.vercel.app/api/',
+  // apiUrl: 'http://localhost:3000/api/',
   enableDebug: true,
 };
