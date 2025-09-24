826828fcef8f799c8de34d3537273fd7682ec030
mangeshkanawade <kanawade84@gmail.com>
2025-09-24 12:43:47 +0530

    Added Hashing to fix 500 issue

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZonelessChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import Lara from '@primeuix/themes/lara';
 
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
@@ -21,7 +21,7 @@ export const appConfig: ApplicationConfig = {
     }),
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes),
+    provideRouter(routes, withHashLocation()),
     provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
   ],
